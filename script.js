4b7f95e70811696e09b17878f6e90bda50087f90
Tusar Chandra Roy <120404181+TusherRoy2@users.noreply.github.com>
2025-07-31 23:42:30 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 let pre_loader = document.querySelector(".pre_loader");
-
 function loader() {
-    pre_loader.style.display = "none";
+    pre_loader.classList.add("none");
+    if (pre_loader.classList.contains("none")) {
+        clearInterval(interval);
+    }
 }
 
 let interval = setInterval(loader, 3000);
